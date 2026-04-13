25d10b74e3ae2b47536a169f8cb6808134df7b78
Sheldon Corcoran <awesomolocity@gmail.com>
2026-04-12 18:37:16 -0700

    Add User-Agent header
    (cherry picked from commit 0b309d3e43fcfd552a5101fa90a486371995e94d)

--- src/GaiaConnector.php
+++ src/GaiaConnector.php
@@ -18,6 +18,7 @@ class GaiaConnector extends Connector
         return [
             'Content-Type' => 'application/json',
             'Accept' => 'application/json',
+            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.10 Safari/605.1.1',
         ];
     }
 }
